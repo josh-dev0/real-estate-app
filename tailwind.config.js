ab1cce6d18b47e4d797db0d3937f81872aad5f74
Josh <joshkay.dev@gmail.com>
2022-10-18 00:34:29 +0800

    configuration updated: tailwindcss, tsconfig, dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        default: "#365272",
+      },
+    },
   },
   plugins: [],
 };
