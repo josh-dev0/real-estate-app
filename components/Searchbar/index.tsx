407024d8533dfcf0658f6a79a1b60bf8e386b72c
Josh <joshkay.dev@gmail.com>
2022-10-21 09:22:39 +0800

    removed unused vars in searchbar

--- components/Searchbar/index.tsx
+++ components/Searchbar/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Button, Checkbox } from 'antd';
 import classNames from 'classnames';
 import { PropertyTypeSelect } from './components/PropertyTypeSelect';
-import { CustomDropdown } from './components/CustomDropdown';
 import { CustomAutoComplete } from './components/CustomAutoComplete';
 import { NumberSelect } from './components/NumberSelect';
 import { PriceSelect } from './components/PriceSelect';
@@ -16,24 +15,6 @@ import {
   SearchOutlined,
 } from '@ant-design/icons';
 import type { IRange } from '../../common/types';
-
-const items = [
-  {
-    label: <a href="https://www.antgroup.com">1st menu item</a>,
-    key: '0',
-  },
-  {
-    label: <a href="https://www.aliyun.com">2nd menu item</a>,
-    key: '1',
-  },
-  {
-    type: 'divider',
-  },
-  {
-    label: '3rd menu item',
-    key: '3',
-  },
-];
 
 type SearchbarProps = {
   locationOptions?: string[];
