130672dd9dfe1bb56693d7d05f572dd876d345fa
Josh <joshkay.dev@gmail.com>
2022-10-24 05:25:27 +0800

    fixed modular style issue in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,6 +11,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    // "@storybook/preset-scss",
     {
       /**
        * Fix Storybook issue with PostCss@8
@@ -30,8 +31,37 @@ module.exports = {
   },
   webpackFinal: (config) => {
     config.module.rules.push({
-      test: /\.scss$/,
-      use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      test: /\.(css|scss)$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            importLoaders: 1, // We always need to apply postcss-loader before css-loader
+            modules: {
+              auto: /(?<!variables)\.module\.scss$/, // true
+              localIdentName: "[name]__[local]--[hash:base64:5]",
+            },
+          },
+        },
+        {
+          loader: "postcss-loader", // required for tailwind
+          options: {
+            implementation: require("postcss"), // postcss 8
+            postcssOptions: {
+              config: path.resolve(__dirname, "../postcss.config.js"),
+            },
+          },
+        },
+        {
+          loader: "sass-loader",
+          options: {
+            // sourceMap: true,
+            // ...sassLoaderOptions,
+            implementation: require("sass"), // dart sass
+          },
+        },
+      ],
       include: path.resolve(__dirname, "../"),
     });
 
