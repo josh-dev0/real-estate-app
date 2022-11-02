4fadf7418c3746476d5058e06bdbf1d6b55641ee
Josh <joshkay.dev@gmail.com>
2022-11-02 21:37:10 +0800

    RegisterForm: updated styles

--- src/components/Auth/RegisterForm/index.tsx
+++ src/components/Auth/RegisterForm/index.tsx
@@ -35,16 +35,16 @@ export const RegisterForm: React.FC<RegisterFormProps> = ({
   };
 
   return (
-    <div className={classNames("bg-primary px-12 py-10", className)}>
-      <p className="text-center text-bg-secondary-dark text-lg leading-[22px]">New here?</p>
+    <div className={classNames(styles.container, className)}>
+      <p className="text-center text-bg-secondary-dark text-lg leading-2xl">New here?</p>
       <NonFormItem>
-        <ul className="text-bg-secondary-dark text-[11px] leading-2xl list-disc mt-8 pl-3">
+        <ul className="text-bg-secondary-dark text-[11px] leading-base list-disc mt-5 pl-3">
           <li>Receive personalised advices for your projects</li>
           <li>Access properties saved in your favorites at any time</li>
         </ul>
       </NonFormItem>
       <Form
-        className="mt-11"
+        className="mt-9"
         {...layout}
         name="nest-messages"
         onFinish={onFinish}
@@ -58,7 +58,7 @@ export const RegisterForm: React.FC<RegisterFormProps> = ({
             iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
           />
         </Form.Item>
-        <NonFormItem className="pl-4 text-xs leading-[22px] text-primary -mt-3">
+        <NonFormItem className="pl-4 text-xs leading-[20px] text-primary -mt-3">
           <div>8 characters minimum</div>
           <div>1 letter minimum</div>
           <div>1 number minimum</div>
@@ -71,15 +71,7 @@ export const RegisterForm: React.FC<RegisterFormProps> = ({
           <Checkbox className="text-sm leading-[22px] text-icon-1">Register to our newsletter</Checkbox>
           <p className="ml-5 text-2xs leadiang-[22px] text-icon-1">You'll be notified with deals, advices and promotions.</p>
         </NonFormItem>
-        <Form.Item
-          className="mt-7"
-          name={['user', 'email']} label="Been referred?" rules={[{ type: 'email' }]}>
-          <Input
-            className="text-2xs leading-[22px]"
-            placeholder="Enter your referral key here"
-          />
-        </Form.Item>
-        <NonFormItem className="text-2xs leading-[22px] text-icon-1">
+        <NonFormItem className="text-2xs leading-[22px] text-icon-1 mt-24 pr-8">
           When clicking on '<i>Create my account</i>', you agree on the {' '}
           <span className="underline"><Link href="">General Terms and Conditions of Use</Link></span>.
         </NonFormItem>
