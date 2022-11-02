8f1c9347ebd89370d19f8add0a0e81f1558fbd86
Josh <joshkay.dev@gmail.com>
2022-11-02 23:55:42 +0800

    InformationForm2: viewed styling

--- src/components/Auth/InformationForm2/index.tsx
+++ src/components/Auth/InformationForm2/index.tsx
@@ -37,13 +37,16 @@ export const InformationForm2: React.FC<InformationForm2Props> = ({
 
   return (
     <Form className={classNames(styles.container, className)}>
-      <p className="text-lg leading-2xl text-bg-secondary-dark">Shall we get acquainted?</p>
-      <ul className="list-disc pl-12 mt-7 text-[11px] leading-2xl text-bg-secondary-dark">
+      <div className="flex items-center justify-between">
+        <p className="text-lg leading-2xl text-bg-secondary-dark">Shall we get acquainted?</p>
+        <p className="underline text-bg-secodary-dark text-xs leading-2xl font-medium">Skip</p>
+      </div>
+      <ul className="list-disc pl-12 mt-4 text-[11px] leading-base text-bg-secondary-dark">
         <li>Save time on your next login</li>
         <li>Start messaging owners when you find your dream house</li>
       </ul>
       <Form.Item
-        className="mt-11"
+        className="mt-10"
         name={['user', 'firstname']}
         rules={[{ required: true }]}
       >
@@ -95,7 +98,7 @@ export const InformationForm2: React.FC<InformationForm2Props> = ({
         />
       </Form.Item>
       <Form.Item
-        name={['user', 'city']}
+        name='city'
         rules={[{ required: true }]}
       >
         <Input
@@ -103,7 +106,7 @@ export const InformationForm2: React.FC<InformationForm2Props> = ({
         />
       </Form.Item>
       <Form.Item
-        name={['user', 'postalCode']}
+        name='postalCode'
         rules={[{ required: true }]}
       >
         <Input
@@ -111,21 +114,20 @@ export const InformationForm2: React.FC<InformationForm2Props> = ({
         />
       </Form.Item>
       <Form.Item
-        name={['user', 'Country']}
-        rules={[{ required: true }]}
-      >
+        name="country"
+        rules={[{ required: true }]}>
         <Select
           className="w-full items-center"
-          loading={false}
-        >
+          placeholder="Country"
+          loading={false}>
           {countries.map(country => <Option key={country.name} value={country.name}>{country.name}</Option>)}
         </Select>
       </Form.Item>
-
       <Button
         className={styles.button}
         block
-      >
+        type="primary"
+        htmlType="submit">
         Let's go!
       </Button>
     </Form>
