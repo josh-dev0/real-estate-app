fafdbdad4056beb4bf1e14083c9d4608d3c39508
Josh <joshkay.dev@gmail.com>
2022-11-14 12:07:59 +0800

    updated TopMenu stories

--- src/_stories_/components/TopMenu.stories.tsx
+++ src/_stories_/components/TopMenu.stories.tsx
@@ -12,16 +12,10 @@ const Template: ComponentStory<typeof TopMenu> = (args) => <TopMenu {...args} />
 
 export const BeforeLogin = Template.bind({});
 BeforeLogin.args = {
-  locale: 'EN/EN',
 };
 
 export const AfterLogin = Template.bind({});
 AfterLogin.args = {
-  locale: 'EN/EN',
-  user: {
-    name: 'Saad',
-    avatar: '',
-  },
   notifications: 5,
 };
 
