058f779b245553330d5b31fe064f10e08ae28d9e
Josh <joshkay.dev@gmail.com>
2022-11-22 03:21:25 +0800

    updated /api/confirm-registration-email

--- src/pages/api/confirm-registration-email.ts
+++ src/pages/api/confirm-registration-email.ts
@@ -1,14 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { request } from "graphql-request";
 import { ROUTES } from "@app/constants";
-import { sleep } from "@app/utils";
+import { VerifyAccountDocument } from "@app/graphql/types";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log("confirm.registration.email.query", req.query);
-  // TODO: process some verification.
-  // finally redirect to login page.
-  await sleep(1000);
-  res.status(301).redirect(ROUTES.login);
+  const { token } = req.query;
+  return request({
+    url: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT!,
+    document: VerifyAccountDocument,
+    variables: {
+      token,
+    },
+  })
+    .then((res) => res.verifyAccount)
+    .then((res) => {
+      if (res.errors) {
+        const keys = Object.keys(res.errors);
+        throw new Error(res.errors[keys[0]][0].message);
+      }
+      return res.status(301).redirect(ROUTES.login);
+    })
+    .catch((err) => {
+      return res.status(400).send(err.message);
+    });
 }
