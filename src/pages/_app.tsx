e07bdbbd05030fa6745b9e39d111fdee2dfb0977
Josh <joshkay.dev@gmail.com>
2022-11-21 12:39:41 +0800

    recoil dev tools environments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,24 +3,29 @@ import { ThemeProvider } from 'next-themes';
 import { SessionProvider } from "next-auth/react"
 import type { Session } from "next-auth"
 import { RecoilRoot } from 'recoil';
+import { RecoilDevTools } from 'recoil-gear'
+// import { EnvironmentKey } from 'recoil-relay';
 import { AntdThemeConfig } from '../containers/AntdThemeConfig';
 import { Preloader } from '@app/containers/Preloader';
 import MyApolloProvider from '../graphql/apollo'
 
 import '../styles/globals.scss'
 
+// const environmentKey = new EnvironmentKey('cozzinest-app');
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps<{ session: Session }>) {
   return (
     <ThemeProvider attribute="data-theme" defaultTheme="light">
       <AntdThemeConfig>
         <SessionProvider session={session}>
-          <MyApolloProvider>
-            <RecoilRoot>
+          <RecoilRoot>
+            <MyApolloProvider>
               <Preloader>
+                <RecoilDevTools />
                 <Component {...pageProps} />
               </Preloader>
-            </RecoilRoot>
-          </MyApolloProvider>
+            </MyApolloProvider>
+          </RecoilRoot>
         </SessionProvider>
       </AntdThemeConfig>
     </ThemeProvider>
