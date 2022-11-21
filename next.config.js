bc62b688fd718cf68dc03d8781182ce08465e2c7
Josh <joshkay.dev@gmail.com>
2022-11-21 12:20:39 +0800

    recoinl issue fixed: duplicated key

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
+const intercept = require("intercept-stdout");
+
+// safely ignore recoil warning messages in dev (triggered by HMR)
+function interceptStdout(text) {
+  if (text.includes("Duplicate atom key")) {
+    return "";
+  }
+  return text;
+}
+
+if (process.env.NODE_ENV === "development") {
+  intercept(interceptStdout);
+}
 
 const nextConfig = {
   reactStrictMode: true,
